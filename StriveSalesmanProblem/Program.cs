78e19da2447d1449a548fbc2118473de4c1a5f15
Daniele Banovaz <daniele.banovaz@gmail.com>
2020-03-04 12:31:08 +0100

    Add project files.

---
+++ StriveSalesmanProblem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace StriveSalesmanProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solving your TSP problem..");
+
+            TspSolver solver = new TspSolver();
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            Path optimalSolution = solver.Solve();
+
+            timer.Stop();
+
+            Console.WriteLine($"\nOptimal solution found for {solver.NumberOfCitiesConsidered} cities:\n{optimalSolution.Description}");
+
+            Console.WriteLine($"That took {timer.ElapsedMilliseconds:#,0}ms");
+        }
+    }
+}
